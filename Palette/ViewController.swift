54b54f34ae7d46384658f003b96e28b74e26da85
PeterHu <peterhu@peterhude-macbook-pro.local>
2018-11-01 20:12:52 +0800

    Finish

--- Palette/ViewController.swift
+++ Palette/ViewController.swift
@@ -10,11 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    @IBOutlet weak var alphaLabel: UILabel!
+    @IBOutlet weak var PandaImageView: UIImageView!
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var alphaSlider: UISlider!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func changeColor(_ sender: UISlider!) {
+        var number = redSlider.value
+        var numberString = ""
+        PandaImageView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: CGFloat(alphaSlider.value))
+        if sender == redSlider {
+            number = redSlider.value
+            numberString = String(format: "%.2f", number)
+            redLabel.text = numberString
+        }
+        if sender == greenSlider {
+            number = greenSlider.value
+            numberString = String(format: "%.2f", number)
+            greenLabel.text = numberString
+        }
+        if sender == blueSlider {
+            number = blueSlider.value
+            numberString = String(format: "%.2f", number)
+            blueLabel.text = numberString
+        }
+        if sender == alphaSlider {
+            number = alphaSlider.value
+            numberString = String(format: "%.2f", number)
+            alphaLabel.text = numberString
+        }
+    }
+    
 }
 
